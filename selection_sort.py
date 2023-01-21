db4312ca03bd7facedf509062cff8d9d2eafb727
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-01-21 17:49:23 +0300

    выборочная сортировка

---
+++ selection_sort.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def selection_sort(_array):
+    count = 0 # Кол-во итераций для сортировки
+
+    array = _array # Массив значений
+    if array == []: # Если массив пуст
+        return None, count
+
+    low = 0 # Начальный индекс массива
+    high = len(array) # Конечный индекс массива
+
+    for i in range(low, high):
+        for j in range(i+1, high):
+            if array[i] > array[j]: # Перестановка элементов
+                array[i], array[j] = array[j], array[i]
+            count += 1
+
+    return array, count
