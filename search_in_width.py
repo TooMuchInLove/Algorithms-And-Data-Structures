501a69342dc1032dc1c25fc76970bb774eb797e3
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-01-26 01:17:26 +0300

    алгоритм поиска в ширину

--- search_in_width.py
+++ search_in_width.py
@@ -12,66 +12,30 @@ def search_in_width(_start, _end, _graph):
     fifo = [] # Очередь для вершин графа
     fifo.append(start)
 
-    if start == end:
-        return fifo
+    # if start == end:
+    #     return fifo
 
-    next_nodes = graph[start]
+    stack = fifo[0]
 
-    print(next_nodes)
-
-    search_in_width(start, end, graph)
-
-    # stack = fifo[0]
-    #
-    # count = 0
-    # # while fifo:
+    count = 0
+    while fifo:
     # while count < 5:
-    #
-    #     # length = len(fifo)
-    #     # if graph[fifo[0]] == 'G': return
-    #     # fifo = graph[fifo[0]] + fifo[0:length]
-    #     # print(fifo, length)
-    #     count += 1
-    #
-    #     # value = stack
-    #     # if value == end:
-    #     #     break
-    #
-    #     print(fifo)
-    #
-    #     next_nodes = []
-    #
-    #     # print(value)
-    #     for next_node in stack:
-    #
-    #         next_nodes = graph[next_node]
-    #
-    #         # print(next_node, next_nodes)
-    #
-    #         # index = fifo.index(next_node)
-    #         # index = len(fifo) - 1
-    #         # fifo = fifo[0:index] + next_nodes
-    #
-    #         # fifo.append(next_nodes)
-    #
-    #         # fifo.append(start)
-    #         if next_node != start:
-    #             fifo.append(next_node)
-    #
-    #
-    #
-    #
-    #         # if next_node not in visited:
-    #         #     fifo.append(next_node)
-    #         #     visited[value] = True
-    #         #     visited[next_node] = False
-    #
-    #     stack = next_nodes
-    #     # print(next_nodes)
-    #     print(fifo)
-    #
-    #     # print(visited)
-    #     #return visited
+        current = fifo[0]
+        print(fifo)
+        #if fifo[0] != start:
+        fifo = fifo[1:]
+        print(fifo)
+        if graph[current] == []:
+            graph[current] = []
+        if graph[current] == [end]:
+            print(True)
+            return True
+        else:
+            fifo = fifo + graph[current]
+
+    print(False)
+    return False
+
 
 
 
