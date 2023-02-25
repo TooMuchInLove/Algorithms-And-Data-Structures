3992b9ed84f6426cd9bd465701b55c726bd831f5
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-25 15:32:19 +0300

    upd main.py

--- main.py
+++ main.py
@@ -7,19 +7,23 @@
 # ----------------------------------------
 #
 from time import time
-
+# Алгоритмы
 from line_search import line_search
 from binary_search import binary_search
 from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 from quick_sort import quick_sort
+from removing_duplicates_from_list_dictionary import removing_duplicates_from_list_dictionary
+from fifo import FiFo
+from lifo import LiFo
+
+from parse_excel import excel
 
 
 if __name__ == '__main__':
     BEGIN = time()
 
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    value = 11
+    array, value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 11
     print('Массив: %s, значение: %d' % (array, value))
 
     index, count = line_search(array, value)
@@ -44,5 +48,28 @@ if __name__ == '__main__':
     array, count = quick_sort(array)
     print('Быстрая сортировка. Кол-во итераций: %d' % (count))
     print('Массив после сортировки: %s' % (array))
+
+    listing = [
+        {"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {},
+        {"key1": "value1"}, {"key1": "value1"},
+        {"key2": "value2"},
+    ]
+    print('Начальный список: %s' % (listing))
+    listing, count = removing_duplicates_from_list_dictionary(0, 0, listing)
+    print('Конечный  список: %s\nКоличество операций: %s' % (listing, count))
+
+    print('Очередь FIFO (first in, first out)')
+    fifo = FiFo()
+    for value in ('A', 'B', 'C', 'D', 'E'):
+        fifo.push(value)
+    fifo.pop()
+
+    print('Стэк LIFO (last in, first out)')
+    lifo = LiFo()
+    for value in ('A', 'B', 'C', 'D', 'E'):
+        lifo.push(value)
+    lifo.pop()
+
+    excel("data1.xlsx")
 
     print('Время выполнения: %s' % (time() - BEGIN))
