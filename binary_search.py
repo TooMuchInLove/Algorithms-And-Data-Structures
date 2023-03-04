8a8dc80ad1d1f82f2806e4f6f2ac1e46ee613503
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-03-04 12:48:35 +0300

    алгоритм бинарного поиска

--- binary_search.py
+++ binary_search.py
@@ -1,29 +1,36 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Аннотация типов
+from typing import List, Tuple
 
-def binary_search(_array, _value):
+# Массив чисел
+IntArray = List[int]
+# Кортеж значений индекса и количества операций
+IndexAndCount = Tuple[int, int]
+
+
+def binary_search(_array: IntArray, _value: int) -> IndexAndCount:
     count = 0 # Кол-во итераций для поиска
+    not_found = -1 # Значение по умолчанию
 
     array = _array # Массив значений
+    if array is []: # Если массив пуст
+        return not_found, count
     value = _value # Искомое значение
-    if array == []: # Если массив пуст
-        return None, count
+    if type(value) is not int: # Если значение НЕ является числом
+        return not_found, count
 
     low = 0 # Начальный индекс массива
     high = len(array)-1 # Конечный индекс массива
-    if array[low] < value > array[high]: # Если значение не входит в массив
-        return None, count
-
-
-    while low <= high:
-        mid = int((low + high)/2)
+    while low <= high: # Выполняем поиск по массиву
         count += 1
+        mid = int((low + high)/2)
         if value == array[mid]:
             return mid, count
         if value < array[mid]:
             high = mid - 1
         if value > array[mid]:
             low = mid + 1
-
-    return None, count
+    # Если значение не найдено
+    count = 0
+    return not_found, count
